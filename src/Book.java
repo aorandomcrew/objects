6fbef10e10b76deecca93dccaaedac032732ee30
aorandomcrew <danbogomolov@gmail.com>
2023-04-22 09:57:32 +0300

    final

---
+++ src/Book.java
@@ -0,0 +1,31 @@
+public class Book {
+    private final String  title;
+
+    private int publicationYear;
+
+    public Author author;
+
+    public Book(String title, int publicationYear, Author author) {
+        this.title = title;
+
+        this.publicationYear = publicationYear;
+
+        this.author = author;
+    }
+
+
+    public final String getTitle() {
+        return title;
+    }
+
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("Rober Kiyosaki");
+        Book book1 = new Book("RICH DAD poor dad", 2005, author1);
+        Author author2 = new Author("Stephen King");
+        Book book2 = new Book("The Shining", 1977, author2);
+        System.out.println("название первой книги: " + book1.getTitle() + ", автор: " + author1.getName() + ", год публикации: " + book1.getPublicationYear());
+        System.out.println();
+        System.out.println("название второй книги: " + book2.getTitle() + ", автор: " + author2.getName() + ", год публикации: " + book2.getPublicationYear());
+        System.out.println();
+        book1.setPublicationYear(2017);
+        System.out.println("Год публикации последнего издания первой книги: " + book1.getPublicationYear());
     }
 }
